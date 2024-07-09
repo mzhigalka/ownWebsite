953b6f5a76e3075079a0e3cf73974b5081f7ed5c
Matvey Zhigalka <mzhigalka@gmail.com>
2024-07-09 13:35:12 +0300

    added new component which copied email adress

---
+++ src/components/CopyMail.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+
+const CopyMail: FC = () => {
+  const [isEmailCopy, setIsEmailCopy] = React.useState<boolean>(false);
+
+  const handleEmailCopy = () => {
+    navigator.clipboard.writeText("mzhigalka@gmail.com").then(() => {
+      setIsEmailCopy(true);
+
+      setTimeout(() => {
+        setIsEmailCopy(false);
+      }, 1500);
+    });
+  };
+
+  return (
+    <div className="mt-[190px]">
+      <p className="text-[22px] text-semigrey  font-light">
+        Get in touch <span className="text-grey">(Click to copy)</span>
+      </p>
+      <div
+        className="cursor-pointer text-[56px] font-semibold text-black"
+        onClick={handleEmailCopy}
+      >
+        {isEmailCopy ? "Email Copied" : "mzhigalka@gmail.com"}
+      </div>
+    </div>
+  );
+};
+
+export default CopyMail;
