e8bd90c5b99afda0f396006e3fd4535e91c400bd
Matvey Zhigalka <mzhigalka@gmail.com>
2024-07-15 11:14:28 +0300

    added component About on page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,18 +2,37 @@ import { FC } from "react";
 
 const About: FC = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-      cupiditate sapiente optio. Molestiae, ipsum similique magni mollitia aut
-      delectus doloribus hic consequatur nulla ut cumque nemo sed blanditiis ex
-      rem eum ipsam, repellendus saepe unde. Necessitatibus quidem dignissimos
-      veniam modi dolorum quod consectetur incidunt libero culpa laudantium
-      sapiente nam, porro dolor ex nesciunt possimus, inventore quisquam
-      similique fugit. Nam, perspiciatis consequuntur laudantium alias vel
-      tempore ab quas tenetur deserunt, minima nobis vitae repudiandae, dicta
-      accusantium eum impedit sed officiis? Voluptatum veniam assumenda impedit
-      aut accusantium ex? Temporibus, molestias tenetur. Veniam consectetur
-      explicabo deserunt suscipit libero, ad in quod nihil nisi?
+    <div className="max-w-[760px] mx-auto">
+      <h1 className="mb-[40px] font-medium text-[44px] leading-[53px] text-black">
+        Senior Product Designer specializing in user-centric innovation and
+        product evolution.
+      </h1>
+      <div className="flex flex-col gap-[40px] font-light text-[22px] text-semigrey leading-[35px]">
+        <p>
+          I'm Dejan, a seasoned Senior Product Designer with over 10 years of
+          experience in dynamic startup environments. Throughout my career, I've
+          led end-to-end design projects, specializing in optimizing user
+          experiences and driving product evolution. I thrive in
+          problem-solving, prioritizing user-centric needs, and delivering
+          high-quality designs that elevate user satisfaction.
+        </p>
+        <p>
+          In previous roles, I've spearheaded strategic initiatives that
+          significantly enhanced product metrics and user engagement. I excel in
+          guiding comprehensive design projects, from strategic planning and
+          user research to UI/UX design and seamless collaboration across teams.
+          With a strong foundation in methodical design thinking and usability
+          principles, I leverage diverse design tools and methodologies to
+          achieve impactful outcomes.
+        </p>
+        <p>
+          My expertise lies in developing intuitive product user interfaces and
+          fostering collaborative environments that integrate design,
+          engineering, and product management. I am dedicated to pushing
+          boundaries and delivering innovative solutions that align with user
+          expectations and achieve business goals.
+        </p>
+      </div>
     </div>
   );
 };
