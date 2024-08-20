287e0e867876b27fd51e0cac6b9f012322212919
Matvey Zhigalka <mzhigalka@gmail.com>
2024-08-20 11:11:22 +0300

    added new images for my portfolio

--- src/assets/constant/projects.ts
+++ src/assets/constant/projects.ts
@@ -1,22 +1,30 @@
 import pizza1 from '../images/projects/pizza1.png'
 import pizza2 from '../images/projects/pizza2.png'
+import pizza3 from '../images/projects/pizza3.png'
 
 import dlnure1 from '../images/projects/dlnure1.png';
 import dlnure2 from '../images/projects/dlnure2.png';
+import dlnure3 from '../images/projects/dlnure3.png';
 
 import airpods1 from '../images/projects/airpods1.png';
 import airpods2 from '../images/projects/airpods2.png';
+import airpods3 from '../images/projects/airpods3.png';
 
 import password1 from '../images/projects/password1.png';
 import password2 from '../images/projects/password2.png';
+import password3 from '../images/projects/password3.png';
 
 import sneakers1 from '../images/projects/sneakers1.png';
 import sneakers2 from '../images/projects/sneakers2.png';
+import sneakers3 from '../images/projects/sneakers3.png';
 
 import watch1 from '../images/projects/watch1.png';
 import watch2 from '../images/projects/watch2.png';
+import watch3 from '../images/projects/watch3.png';
 
 import crm1 from '../images/projects/crm1.png';
+import crm2 from '../images/projects/crm2.png';
+import crm3 from '../images/projects/crm3.png';
 
 export const projectsComponents = [
   {
@@ -24,48 +32,48 @@ export const projectsComponents = [
     title: "Pizza Delivery",
     text: 'Pizza Delivery is a convenient app that makes every order special. Delicious pizza is not just food, it is a small joy that brightens your day. Whether it is a cozy night at home, a party with friends, or an important event, our pizza is always the perfect choice.',
     description: '',
-    images: [pizza1, pizza2],
+    images: [pizza1, pizza2, pizza3],
   },
   {
     id: "dlnure",
     title: "DlNure Booking",
     text: 'DLNure Booking is a seamless platform that connects you to the best hotels for your perfect stay. Whether it is a luxurious getaway, a business trip, or a weekend escape, finding the ideal accommodation is effortless. With DLNure Booking, your next adventure is just a click away.',
     description: '',
-    images: [dlnure1, dlnure2],
+    images: [dlnure1, dlnure2, dlnure3],
   },
   {
     id: "airpods",
     title: "AirPods Lending",
     text: 'AirPods Lending is your one-stop shop for the latest AirPods Pro. Whether you are upgrading your sound experience or trying out the latest tech, we have got the perfect pair for you.Discover seamless connectivity, top- tier sound quality, and a new way to enjoy your favorite audio.',
     description: '',
-    images: [airpods1, airpods2],
+    images: [airpods1, airpods2, airpods3],
   },
   {
     id: "password",
     title: "Random Password Generator",
     text: 'Random Password Generator is your reliable tool for creating secure and unique passwords. Whether you need protection for your accounts or extra security for your data, generate strong passwords with just a click. Keep your information safe with ease.',
     description: '',
-    images: [password1, password2],
+    images: [password1, password2, password3],
   },
   {
     id: "sneakers",
     title: "Sneakers Shop",
     text: 'Sneakers Shop is your go-to destination for the latest in stylish sneakers. Whether you are looking for exclusive releases, timeless classics, or trendy designs, we have got you covered.Step up your sneaker game and find the perfect pair for any occasion.',
     description: '',
-    images: [sneakers1, sneakers2],
+    images: [sneakers1, sneakers2, sneakers3],
   },
   {
     id: "watch-store",
     title: "Luxury Watch Store",
     text: 'Luxury Watch Store is your destination for timeless elegance and premium craftsmanship. Explore a curated selection of luxury watches that embody sophistication and style. Whether for a special occasion or everyday wear, find the perfect timepiece to elevate your look.',
     description: '',
-    images: [watch1, watch2],
+    images: [watch1, watch2, watch3],
   },
   {
     id: "abi-crm",
     title: "Abi CRM System",
     text: 'Luxury Watch Store is your destination for timeless elegance and premium craftsmanship. Explore a curated selection of luxury watches that embody sophistication and style. Whether for a special occasion or everyday wear, find the perfect timepiece to elevate your look.',
     description: '',
-    images: [crm1],
+    images: [crm1, crm2, crm3],
   },
 ];
