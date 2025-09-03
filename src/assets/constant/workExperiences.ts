92ff6bf7d7efe30dfcf2247094cc8f5867ba9b37
mzhigalka <mzhigalka@gmail.com>
2025-09-03 19:12:37 +0300

    updated about info and changed workExperiences object

--- src/assets/constant/workExperiences.ts
+++ src/assets/constant/workExperiences.ts
@@ -1,10 +1,5 @@
 export const workExperiences = [
     {
-        id: 1,
-        position: 'React Developer',
-        company: 'Arctic Web',
-    },
-    {
         id: 2,
         position: 'Intern Frontend Developer',
         company: 'HARBOUR TECH ABI CRM LTD',
@@ -13,5 +8,10 @@ export const workExperiences = [
         id: 3,
         position: 'Frontend Developer',
         company: 'HARBOUR TECH ABI CRM LTD',
+    },
+    {
+        id: 1,
+        position: 'React Developer',
+        company: 'Arctic Web',
     },
 ];
