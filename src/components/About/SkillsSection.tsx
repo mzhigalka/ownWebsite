cfd232b71c6af9fed3056ad5fb261529f8f78767
mzhigalka <mzhigalka@gmail.com>
2025-12-24 21:39:14 +0200

    update SkillsSection

--- src/components/About/SkillsSection.tsx
+++ src/components/About/SkillsSection.tsx
@@ -11,23 +11,6 @@ const SkillsSection: FC<SkillsSectionProps> = ({ t }) => {
             <h3 className="text-[32px] font-medium text-black mb-[20px] max-md:text-2xl max-md:font-semibold">
                 {t('about.skills.title')}
             </h3>
-            {/* <div className="grid grid-cols-2 text-[22px] text-semigrey font-light leading-[35px] mb-[80px] max-md:grid-cols-1 max-md:text-lg max-md:font-normal">
-                <p>HTML</p>
-                <p>CSS / SCSS / SASS / LESS</p>
-                <p>React JS</p>
-                <p>Git / GitHub / GitLab</p>
-                <p>JavaScript</p>
-                <p>Tailwind CSS</p>
-                <p>TypeScript</p>
-                <p>Figma</p>
-                <p>Redux Toolkit</p>
-                <p>Vite</p>
-                <p>Node JS</p>
-                <p>REST API</p>
-                <p>Express JS</p>
-                <p>Postman</p>
-                <p>MongoDB</p>
-            </div> */}
 
             <div className="grid grid-cols-2 gap-y-4 text-[22px] text-semigrey font-light leading-[35px] mb-[80px] max-md:grid-cols-1 max-md:text-lg max-md:font-normal">
                 <div>
@@ -37,6 +20,7 @@ const SkillsSection: FC<SkillsSectionProps> = ({ t }) => {
                     <p>JavaScript</p>
                     <p>TypeScript</p>
                     <p>React JS</p>
+                    <p>Next JS</p>
                     <p>Redux Toolkit</p>
                     <p>Tailwind CSS</p>
                     <p>Vite</p>
@@ -44,10 +28,11 @@ const SkillsSection: FC<SkillsSectionProps> = ({ t }) => {
                 <div>
                     <h4 className="font-medium text-black mb-2">Back-end</h4>
                     <p>Node JS</p>
+                    <p>Nest JS</p>
                     <p>Express JS</p>
-                    <p>MongoDB</p>
                     <p>REST API</p>
                     <p>Postman</p>
+                    <p>MongoDB / Mongoose</p>
                     <p>JWT (Authentication & Authorization)</p>
                 </div>
                 <div>
